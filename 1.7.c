435615467cae1e3910f68f3766b4a3252939f6bb
AntoineC <antoinecraipeau@outlook.fr>
2025-02-27 16:35:04 +0100

    1.7

--- 1.7.c
+++ 1.7.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+/** Code fourni :
+unsigned int i, nrep;
+nrep = packet_get_int();
+response = malloc(nrep * sizeof(char*));
+if (response != NULL)
+    for (i = 0; i < nrep; i++)
+        response[i] = packet_get_string(NULL);
+*/
+
+unsigned int i, nrep;
+nrep = packet_get_int();
+
+// On vérifie que { nrep * (taille d'un pointeur) } ne cause pas un wrap.
+if (nrep > SIZE_MAX / sizeof(char*)) {
+    // Erreur : dépassement potentiel
+    return -EFBIG;
 }
+
+response = malloc(nrep * sizeof(char*));
+if (response != NULL)
+    for (i = 0; i < nrep; i++)
+        response[i] = packet_get_string(NULL);
